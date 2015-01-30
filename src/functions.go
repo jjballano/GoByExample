f41efece20970cc8394b61e05a307e5f07a40f9e
Jesús Jiménez Ballano <jjballano@gmail.com>
2015-01-30 07:41:09 +0000

    Variadic function added

--- src/functions.go
+++ src/functions.go
@@ -14,6 +14,14 @@ func main() {
 	fmt.Println("-----")
 
 	fmt.Println("We could use _,sum:=multipleReturnValues(1, 2) if we don't care about the first result")
+
+	fmt.Println("-----")
+	fmt.Println("A Variadic Function can be called with any number of arguments ")
+	fmt.Println("variadicFunction(1,2,3) => ",variadicFunction(1,2,3))
+	fmt.Println("variadicFunction(1,2,3,4) => ",variadicFunction(1,2,3,4))
+	nums := []int{4,5,6,7}
+	fmt.Println("Can also be called with an array but adding ... after the param ")
+	fmt.Println("variadicFunction(nums...) => ",variadicFunction(nums...))
 }
 
 func singleReturnValue(num1 int, num2 int) int{
@@ -23,4 +31,12 @@ func singleReturnValue(num1 int, num2 int) int{
 func multipleReturnValues(num1 int, num2 int)(string, int){
 	sum := num1 + num2
 	return "The sum of the params is",sum
+}
+
+func variadicFunction(nums ...int) int {
+	sum := 0
+	for _,num := range nums {
+		sum += num
+	}
+	return sum
 }
