960c327d75a05d0efcfbf17d0c0c9d17f1f3a5e6
Jesús Jiménez Ballano <jjballano@gmail.com>
2015-02-02 07:08:44 +0000

    Buffered channels added

--- src/channels.go
+++ src/channels.go
@@ -6,12 +6,22 @@ func main() {
 	fmt.Println("\"make\" creates a new channel ")
 	messages := make(chan string)
 
+	//Channels buffered
+	buffered := make(chan string, 2)
+
 	go func(){
 		fmt.Println("Program waits until channel is written")
 		messages <- "ping"
+
+		buffered <- "First"
+		buffered <- "Second"
 	}()
 
 	msg := <- messages
-	fmt.Println("Program waits until channel is read")
+	msg += <- buffered
+	msg += <- buffered
+
+	fmt.Println("Program waits until channels are read")
+	fmt.Println("For any reason, it doesnt need to wait for buffered channels")
 	fmt.Println(msg)
 }
