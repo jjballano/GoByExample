dcf78bb354d4b14d1ac48bd019d9fe162aa9bab6
Jesus Jimenez Ballano <jjballano@gmail.com>
2015-02-19 13:44:29 +0000

    Base64 section

---
+++ src/base64.go
@@ -0,0 +1,17 @@
+package main 
+
+import ("fmt"
+		b64 "encoding/base64")
+
+func main() {
+	data := "abc123!?$*&()'-=@~"
+    sEnc := b64.StdEncoding.EncodeToString([]byte(data))
+    fmt.Println(sEnc)
+    sDec, _ := b64.StdEncoding.DecodeString(sEnc)
+    fmt.Println(string(sDec))
+    fmt.Println("now using a URL-compatible base64 format")
+    uEnc := b64.URLEncoding.EncodeToString([]byte(data))
+    fmt.Println(uEnc)
+    uDec, _ := b64.URLEncoding.DecodeString(uEnc)
+    fmt.Println(string(uDec))
+}		
